2bc80b631a90fbb05194538b4bce85ff0a267023
Titouan Raimbault <t_raimbault@yahoo.fr>
2020-09-14 16:20:18 +0200

    #WA-46 : Configure discovery for Eureka Server

--- src/main/java/com/wildadventure/trip/TripApplication.java
+++ src/main/java/com/wildadventure/trip/TripApplication.java
@@ -2,6 +2,7 @@ package com.wildadventure.trip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,6 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableFeignClients("com.wildadventure")
+@EnableDiscoveryClient
 public class TripApplication {
 
 	public static void main(String[] args) {
